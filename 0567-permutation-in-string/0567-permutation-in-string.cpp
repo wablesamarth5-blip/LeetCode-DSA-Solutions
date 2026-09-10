fb518ad6eb6ce5a4f4d1482408902644dbf728c5
Samarth Wable <wablesamarth5@gmail.com>
2026-09-10 16:15:03 +0530

    Time: 4 ms (52.87%), Space: 9.8 MB (98.75%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,48 +1,50 @@
-
-#include <string>
-#include <vector>
-
 class Solution {
 public:
-    bool checkInclusion(std::string s1, std::string s2) {
-        int n1 = s1.length(), n2 = s2.length();
-        if (n1 > n2) return false;
+    bool checkInclusion(string s1, string s2) {
+        
+    if(s1.length()>s2.length())
+    {
+        return false;
+    }
 
-        std::vector<int> s1Count(26, 0);
-        std::vector<int> s2Count(26, 0);
 
-        for (int i = 0; i < n1; i++) {
-            s1Count[s1[i] - 'a']++;
-            s2Count[s2[i] - 'a']++;
+    int windowsize=s1.length();
+    vector<int> freq1 (26,0);
+    vector<int> freq2 (26,0);
+
+    for(int i=0;i<s1.length();i++)
+    {
+        freq1[s1[i]-'a']++;
+    }
+
+    //first window
+
+    for(int i=0;i<windowsize;i++)
+    {
+        freq2[s2[i]-'a']++;
+    }
+    
+    
+    if(freq1==freq2)
+    {
+        return true;
+     }
+    //remaining windows
+
+    int left=0;
+    for(int right=windowsize;right<s2.length();right++)
+    {
+        freq2[s2[right]-'a']++;
+
+        freq2[s2[left]-'a']--;
+
+        left++;
+
+        if(freq1==freq2)
+        {
+            return true;
         }
-
-        int matches = 0;
-        for (int i = 0; i < 26; i++) {
-            if (s1Count[i] == s2Count[i]) matches++;
-        }
-
-        for (int i = 0; i < n2 - n1; i++) {
-            if (matches == 26) return true;
-
-            // Character entering the window
-            int r = s2[i + n1] - 'a';
-            s2Count[r]++;
-            if (s2Count[r] == s1Count[r]) {
-                matches++;
-            } else if (s2Count[r] == s1Count[r] + 1) {
-                matches--;
-            }
-
-            // Character leaving the window
-            int l = s2[i] - 'a';
-            s2Count[l]--;
-            if (s2Count[l] == s1Count[l]) {
-                matches++;
-            } else if (s2Count[l] == s1Count[l] - 1) {
-                matches--;
-            }
-        }
-
-        return matches == 26;
+    }
+  return false;
     }
 };
