b73e022175c5846dcae2cfe8ebc84d57b56c4973
Samarth Wable <wablesamarth5@gmail.com>
2026-09-25 17:59:11 +0530

    Time: 0 ms (100%), Space: 10.4 MB (88.38%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+    void getPerms(vector<int>&nums,int idx,vector<vector<int>>&ans)
+    {  
+        if(idx==nums.size())
+        {
+            ans.push_back(nums);
+            return;
+        }
+
+        for(int i=idx;i<nums.size();i++)
+        {
+            swap(nums[idx],nums[i]);
+            getPerms(nums,idx+1,ans);
+            swap(nums[idx],nums[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        
+        vector<vector<int>>ans;
+        getPerms(nums,0,ans);
+        return ans;
+
+    }
+};
